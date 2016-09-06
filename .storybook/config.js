b6ddbd451dcdaf9806c7b68f82d41e0d513ce952
elyseko <elyse1213+github@gmail.com>
2016-09-05 20:50:02 -0700

    Remove hello world examples and rename chapter_3_2 story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
 import { configure } from '@kadira/storybook';
-import styles from 'css!../components/styles.css'
+import styles from '../components/styles.css'
 
 function loadStories() {
-  require('../components/stories/Chapter_3_2/helloWorldStory.js');
+  require('../components/stories/Chapter_3_2/chapter_3_2_examples.js');
   require('../components/stories/todo/todoStory.js');
   // require as many stories as you need.
 }
--- components/stories/Chapter_3_2/chapter_3_2_examples.js
+++ components/stories/Chapter_3_2/chapter_3_2_examples.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import HelloWorld from '../../Chapter_3_2/helloWorld.jsx';
-import HelloWorldVar from '../../Chapter_3_2/helloWorldVar.jsx';
 import Link from '../../Chapter_3_2/link.jsx';
 import ItemFunctional from '../../Chapter_3_2/itemFunctional.jsx';
+import ButtonBasic from '../../Chapter_3_2/buttonBasic.jsx';
 
-storiesOf('Hello World, 3.2.1', module)
-  .add('example 1, with text', () => (
-    <HelloWorld/>
-  ))
-  .add('example 2, add a variable', () => (
-    <HelloWorldVar/>
+storiesOf('Chapter 3.2 examples', module)
+  .add('JSX basic button', () => (
+    <ButtonBasic/>
   ))
   .add('link displays with props', () => (
     <Link displayName="My first link"
