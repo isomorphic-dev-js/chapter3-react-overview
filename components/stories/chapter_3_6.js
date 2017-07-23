e13e0d9c43e1d9dd15fe75f090c553d42554966e
elyseko <elyse1213+github@gmail.com>
2017-07-22 22:19:56 -0700

    chore: remove code that is no longer in the chapter

--- components/stories/chapter_3_6.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import Lifecycle from '../Chapter_3_6/lifecycle.jsx';
-
-storiesOf('Chapter 3.6 examples', module)
-  .add('Lifecycle example', () => (
-    <Lifecycle actions={{lifecycleEvent: action('lifecycleEvent')}}/>
-  ));
