aac2a664a98da30b895df71d507c49af40ca8fbe
elyseko <elyse1213+github@gmail.com>
2017-07-22 22:19:56 -0700

    chore: remove code that is no longer in the chapter

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import styles from '../components/styles.css'
 
 function loadStories() {
   require('../components/stories/chapter_3_4.js');
-  require('../components/stories/chapter_3_6.js');
   require('../components/stories/todo.js');
   // require as many stories as you need.
 }
