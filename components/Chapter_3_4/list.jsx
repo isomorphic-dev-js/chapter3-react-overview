9f998a2c6ed903131a52970dcd462758a4fe2417
elyseko <elyse1213+github@gmail.com>
2017-09-27 00:12:34 -1000

    fix: make List a const

--- components/Chapter_3_4/list.jsx
+++ components/Chapter_3_4/list.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Item from './itemDone.jsx'
 
-let List = (props) => {
+const List = (props) => {
   let listItems = [];
   props.data.forEach((item)=>{
     listItems.push(<Item key={item.id} {...item} actions={props.actions}/>);
